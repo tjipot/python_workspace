9655a28fa589ee8cd106bfd4dba3400dd0ee0d32
HY <tomjackinpot@163.com>
2017-09-16 08:23:34 +0800

    0916_SYN_H

---
+++ psn_demo/cs231n/dictionary_comprehension.py
@@ -0,0 +1,7 @@
+nums = [0, 1, 2, 3, 4]
+even_num_to_square = {x: x ** 2 for x in nums if x % 2 == 0}
+print(even_num_to_square)
+
+# /Library/Frameworks/Python.framework/Versions/3.5/bin/python3.5
+#   + /Users/haoranye/Documents/python_workspace/psn_demo/cs231n/dictionary_comprehension.py
+# {0: 0, 2: 4, 4: 16}
